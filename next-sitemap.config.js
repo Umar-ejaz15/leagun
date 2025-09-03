5ca41269cd5d3f9f5ebe6e97c6cfef02463ccf16
umar ejaz <umar.senpaiiii@gmail.com>
2025-09-03 19:49:09 +0500

    leagun final

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://leagun.com',
-  generateRobotsTxt: true, // optional but recommended
-  changefreq: 'daily',
-  priority: 1.0, // your main page is important
-};
